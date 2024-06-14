82bc0b47fae4b00e3cc853646665c34ef35bfef1
Michael Rodriguez <mdr721.edu@gmail.com>
2024-06-13 20:41:23 -0400

    1. Added Shader class help function 2. Updated OpenGL Version 4.1 3. Updated LevelEditorScene to use the Shader class

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -2,6 +2,7 @@ package jade;
 
 
 import org.lwjgl.BufferUtils;
+import renderer.Shader;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -14,29 +15,9 @@ public class LevelEditorScene extends Scene {
 
     private boolean changingScene = false;
     private float timeChangeScene = 2.0f;
+    private Shader defaultShader;
 
-    private String vertexShaderSrc = "#version 330 core\n" +
-            "layout (location=0) in vec3 aPos;\n" +
-            "layout (location=1) in vec4 aColor;\n" +
-            "\n" +
-            "out vec4 fColor;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    fColor = aColor;\n" +
-            "    gl_Position = vec4(aPos, 1.0);\n" +
-            "}";
 
-    private String fragmentShaderSrc = "#version 330 core\n" +
-            "\n" +
-            "in vec4 fColor;\n" +
-            "\n" +
-            "out vec4 color;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    color = fColor;\n" +
-            "}";
 
     private int vertexID, fragmentID, shaderID,shaderProgram;
 
@@ -58,118 +39,31 @@ public class LevelEditorScene extends Scene {
     private int vaoID, vboID, eboID;
 
     public LevelEditorScene() {
-
+        Shader test = new Shader("assets/shaders/default.glsl");
     }
 
     @Override
     public void init() {
-        // ====================================
-        // Compile and Link shader
-        // ====================================
-
-        // First load and compile vertex shader
-
-        vertexID = glCreateShader(GL_VERTEX_SHADER);
-        // pass shader source to the GPU
-        glShaderSource(vertexID, vertexShaderSrc);
-        glCompileShader(vertexID);
-
-        //check for error, in compilation
-        int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
-        if (success == GL_FALSE) {
-            int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
-            System.out.println("ERROR: 'default.glsl' v \n\t Vertex shader compilation error: " + glGetShaderInfoLog(vertexID, len));
-            assert false : "";
-
-        }
 
 
-        fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
-        // pass shader source to the GPU
-        glShaderSource(fragmentID, fragmentShaderSrc);
-        glCompileShader(fragmentID);
-
-        //check for error, in comilation
-        success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
-        if (success == GL_FALSE) {
-            int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
-            System.out.println("ERROR: 'default.glsl' f\n\t Fragment shader compilation error: " + glGetShaderInfoLog(fragmentID, len));
-            assert false : "";
-
-        }
-
-        //check for linking errors
-        shaderProgram = glCreateProgram();
-        glAttachShader(shaderProgram, vertexID);
-        glAttachShader(shaderProgram, fragmentID);
-        glLinkProgram(shaderProgram);
-
-        success = glGetProgrami(shaderProgram, GL_LINK_STATUS);
-
-        if (success == GL_FALSE) {
-            int len = glGetProgrami(shaderProgram, GL_INFO_LOG_LENGTH);
-            System.out.println("ERROR: 'default.glsl' p\n\t Linking of shader compilation error: " + glGetProgramInfoLog(fragmentID, len));
-            assert false : "";
-        }
 
         // ============================================================
         // Generate VAO, VBO, and EBO buffer objects, and send to GPU
         // ============================================================
 
-        vaoID = glGenVertexArrays();
-        glBindVertexArray(vaoID);
-
-        //create a float buffer of vertices
-        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
-        vertexBuffer.put(vertexArray).flip();
-
-        // Create VBO upload the vertex buffer
-        vboID = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboID);
-        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
-
-        // Create the indices and upload
-        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
-        elementBuffer.put(elementArray).flip();
-
-        eboID = glGenBuffers();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
-
-        //Add vertex attributes pointer
-        int positionSize = 3;
-        int colorSize = 4;
-        int floatSizeBytes = 4;
-        int vertexSizeBytes = (positionSize + colorSize) * floatSizeBytes;
-        glVertexAttribPointer(0,positionSize,GL_FLOAT,false, vertexSizeBytes, 0);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1,colorSize,GL_FLOAT,false,vertexSizeBytes,positionSize * floatSizeBytes);
-        glEnableVertexAttribArray(1);
-
-
-
+        defaultShader = new Shader("assets/shaders/default.glsl");
+        defaultShader.compile();
 
 
     }
 
     @Override
     public void update(float dt) {
-        //bind shader program
-        glUseProgram(shaderProgram);
-        //BInd the VAO
-        glBindVertexArray(vaoID);
-        //Enable the vertex attribute pointer
-        glEnableVertexAttribArray(0);
-        glEnableVertexAttribArray(1);
-
-        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
-
-        glBindVertexArray(0);
-        glUseProgram(0);
+       defaultShader.use();
+       glBindVertexArray(vaoID);
+       glBindVertexArray(vboID);
+       glBindVertexArray(0);
+       defaultShader.detach();
     }
 
 
--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -88,8 +88,8 @@ public class Window {
         glfwWindowHint(GLFW_VISIBLE,GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
         glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
